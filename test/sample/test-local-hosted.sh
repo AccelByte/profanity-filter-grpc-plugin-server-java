0e01fd065d182ea6a225ec74472adbef7c6cd62e
Dhanar Adi Dewandaru <dhanar@accelbyte.net>
2024-07-09 10:24:07 +0700

    test(demo): use random filter name

--- test/sample/test-local-hosted.sh
+++ test/sample/test-local-hosted.sh
@@ -27,23 +27,12 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-echo '# Check environment variables'
-
-variables=(AB_BASE_URL AB_CLIENT_ID AB_CLIENT_SECRET AB_NAMESPACE AB_USERNAME AB_PASSWORD NGROK_AUTHTOKEN)
-
-for variable_name in "${variables[@]}"; do
-  if [ -z "${!variable_name}" ]; then
-    echo "Variable $variable_name is empty"
-    exit 1
-  fi
-done
-
 echo '# Run ngrok'
 
 ( ngrok tcp 6565 > ngrok.log 2>&1 ) & NGROK_PID=$!
 
 for _ in {1..12}; do
-  sleep 10
+  sleep 5s
   NGROK_RESPONSE=$(curl -s --location 'localhost:4040/api/tunnels')
   NGROK_URL=$(echo "$NGROK_RESPONSE" | jq -r '.tunnels[] | select(.config.addr = "localhost:6565") | .public_url')
   if [ -n "$NGROK_URL" ]; then
@@ -59,4 +48,3 @@ fi
 echo '# Testing Extend app using demo script'
 
 GRPC_SERVER_URL="${NGROK_URL#*://}" bash demo.sh
-
